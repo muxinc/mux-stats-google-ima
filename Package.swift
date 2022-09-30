45316190427421f8587a368b4cdde0ad0ffb7cf2
Nidhi Kulkarni <nkulkarni@mux.com>
2022-09-30 14:00:11 -0400

    Support SwiftPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "MUXSDKImaListener",
+    products: [
+        .library(name: "MUXSDKImaListener", targets: ["MUXSDKImaListener"]),
+    ],
+    dependencies: [
+        .package(
+            name: "MUXSDKStats",
+            url: "https://github.com/muxinc/mux-stats-sdk-avplayer.git",
+            from: "2.13.0"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "MUXSDKImaListener",
+            dependencies: ["MUXSDKStats"]),
+    ]
+)
