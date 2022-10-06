758ffaf560c758061256941ba5eeeeaaef1abd53
Nidhi Kulkarni <nkulkarni@mux.com>
2022-10-06 00:11:12 -0400

    try again

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "Mux_Stats_Google_IMA",
-            dependencies: ["MUXSDKStats"],
+            dependencies: ["MUXSDKStats", "GoogleInteractiveMediaAds"],
             path: "MUXSDKImaListener/Classes"
         ),
         .binaryTarget(
