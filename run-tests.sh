0dc5406aae5fb160eb3d1ce9c4da8f72e7b82ced
Emily Dixon <edixon@mux.com>
2022-12-01 12:41:23 -0800

    Run unit tests too

--- run-tests.sh
+++ run-tests.sh
@@ -14,6 +14,11 @@ pushd Example
 #pod deintegrate && pod update
 
 xcodebuild -workspace MUXSDKImaListener.xcworkspace \
+           -scheme "MUXSDKImaListener-Example" \
+           -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0' \
+           test \
+           | xcbeautify
+xcodebuild -workspace MUXSDKImaListener.xcworkspace \
            -scheme "DemoApp" \
            -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0' \
            test \
