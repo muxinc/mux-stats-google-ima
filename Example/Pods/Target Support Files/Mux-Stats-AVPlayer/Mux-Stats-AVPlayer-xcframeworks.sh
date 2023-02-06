ab3b79c19790ba6d52d2ae0454e4f80d5146e00e
Emily Dixon <edixon@mux.com>
2023-02-06 13:46:16 -0800

    Update pods

--- Example/Pods/Target Support Files/Mux-Stats-AVPlayer/Mux-Stats-AVPlayer-xcframeworks.sh
+++ Example/Pods/Target Support Files/Mux-Stats-AVPlayer/Mux-Stats-AVPlayer-xcframeworks.sh
@@ -17,19 +17,19 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "MUXSDKStats.xcframework/tvos-arm64_x86_64-simulator")
-    echo "simulator"
-    ;;
   "MUXSDKStats.xcframework/tvos-arm64")
+    echo ""
+    ;;
+  "MUXSDKStats.xcframework/ios-arm64")
     echo ""
     ;;
   "MUXSDKStats.xcframework/ios-arm64_x86_64-maccatalyst")
     echo "maccatalyst"
     ;;
-  "MUXSDKStats.xcframework/ios-arm64")
-    echo ""
-    ;;
   "MUXSDKStats.xcframework/ios-arm64_x86_64-simulator")
+    echo "simulator"
+    ;;
+  "MUXSDKStats.xcframework/tvos-arm64_x86_64-simulator")
     echo "simulator"
     ;;
   esac
@@ -38,19 +38,19 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "MUXSDKStats.xcframework/tvos-arm64_x86_64-simulator")
-    echo "arm64 x86_64"
-    ;;
   "MUXSDKStats.xcframework/tvos-arm64")
+    echo "arm64"
+    ;;
+  "MUXSDKStats.xcframework/ios-arm64")
     echo "arm64"
     ;;
   "MUXSDKStats.xcframework/ios-arm64_x86_64-maccatalyst")
     echo "arm64 x86_64"
     ;;
-  "MUXSDKStats.xcframework/ios-arm64")
-    echo "arm64"
-    ;;
   "MUXSDKStats.xcframework/ios-arm64_x86_64-simulator")
+    echo "arm64 x86_64"
+    ;;
+  "MUXSDKStats.xcframework/tvos-arm64_x86_64-simulator")
     echo "arm64 x86_64"
     ;;
   esac
@@ -135,5 +135,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/Mux-Stats-AVPlayer/XCFramework/MUXSDKStats.xcframework" "Mux-Stats-AVPlayer" "framework" "ios-arm64_x86_64-maccatalyst" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/Mux-Stats-AVPlayer/XCFramework/MUXSDKStats.xcframework" "Mux-Stats-AVPlayer" "framework" "ios-arm64" "ios-arm64_x86_64-maccatalyst" "ios-arm64_x86_64-simulator"
 
