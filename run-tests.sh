f47d3fd971a52d52cddc0eb305d2c4b1f674dbdc
Emily Dixon <edixon@mux.com>
2023-02-06 17:13:41 -0800

    Try a simulator-related fix

--- run-tests.sh
+++ run-tests.sh
@@ -18,7 +18,11 @@ xcodebuild -workspace MUXSDKImaListener.xcworkspace \
            -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
            test \
            | xcbeautify
-xcrun -v simctl shutdown all # The simulator seems to crash without this
+#xcrun -v simctl shutdown all # The simulator seems to crash without this
+
+# https://circleci.com/blog/xcodebuild-exit-code-65-what-it-is-and-how-to-solve-for-ios-and-macos-builds/
+xcrun instruments -w 'iPhone 14 (16.2)' || sleep 15
+
 xcodebuild -workspace MUXSDKImaListener.xcworkspace \
            -scheme "DemoApp" \
            -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
