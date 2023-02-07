f42ddeefef74b5871476de4c6d7f795bfa75c944
Emily Dixon <edixon@mux.com>
2023-02-07 11:51:30 -0800

    Releases/v0.10.0 (#24)
    ## Improvements
    
    * Track Deeper Ad Metadata, on a per-event basis (#23)

--- Example/Pods/GoogleAds-IMA-iOS-SDK/GoogleInteractiveMediaAds.xcframework/ios-arm64_armv7/GoogleInteractiveMediaAds.framework/Headers/IMAPodStreamRequest.h
+++ Example/Pods/GoogleAds-IMA-iOS-SDK/GoogleInteractiveMediaAds.xcframework/ios-arm64_armv7/GoogleInteractiveMediaAds.framework/Headers/IMAPodStreamRequest.h
@@ -23,13 +23,15 @@ NS_ASSUME_NONNULL_BEGIN
  * @param customAssetKey The asset key for the stream.
  * @param adDisplayContainer The IMAAdDisplayContainer for rendering the ad UI.
  * @param videoDisplay The IMAVideoDisplay where the stream will be played.
+ * @param userContext The user context for tracking requests (optional)
  *
  * @return The IMAPodStreamRequest instance.
  */
 - (instancetype)initWithNetworkCode:(NSString *)networkCode
                      customAssetKey:(NSString *)customAssetKey
                  adDisplayContainer:(IMAAdDisplayContainer *)adDisplayContainer
-                       videoDisplay:(id<IMAVideoDisplay>)videoDisplay;
+                       videoDisplay:(id<IMAVideoDisplay>)videoDisplay
+                        userContext:(nullable id)userContext;
 
 /**
  * Initializes a stream request instance with the given network code and custom
@@ -41,6 +43,7 @@ NS_ASSUME_NONNULL_BEGIN
  * @param adDisplayContainer The IMAAdDisplayContainer for rendering the ad UI.
  * @param videoDisplay The IMAVideoDisplay where the stream will be played.
  * @param pictureInPictureProxy The IMAPictureInPictureProxy for tracking PIP events.
+ * @param userContext The user context for tracking requests (optional)
  *
  * @return The IMAPodStreamRequest instance.
  */
@@ -48,7 +51,8 @@ NS_ASSUME_NONNULL_BEGIN
                      customAssetKey:(NSString *)customAssetKey
                  adDisplayContainer:(IMAAdDisplayContainer *)adDisplayContainer
                        videoDisplay:(id<IMAVideoDisplay>)videoDisplay
-              pictureInPictureProxy:(nullable IMAPictureInPictureProxy *)pictureInPictureProxy;
+              pictureInPictureProxy:(nullable IMAPictureInPictureProxy *)pictureInPictureProxy
+                        userContext:(nullable id)userContext;
 
 /**
  * :nodoc:
--- run-tests.sh
+++ run-tests.sh
@@ -15,12 +15,13 @@ pushd Example
 
 xcodebuild -workspace MUXSDKImaListener.xcworkspace \
            -scheme "MUXSDKImaListener-Example" \
-           -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0' \
+           -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
            test \
            | xcbeautify
+
 xcodebuild -workspace MUXSDKImaListener.xcworkspace \
            -scheme "DemoApp" \
-           -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.0' \
+           -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.2' \
            test \
            | xcbeautify
 
