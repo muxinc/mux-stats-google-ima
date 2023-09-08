2c8cbf19e87d5e0eb0387797a5726ceab227cd9e
AJ Lauer Barinov <abarinov@mux.com>
2023-09-08 11:22:19 -0700

    ci: clean-up test script

--- run-tests.sh
+++ run-tests.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
+
+readonly XCODE_VERSION=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
+
+readonly SCHEME=DemoApp
+
+readonly TOP_DIR=$(pwd)
+
+readonly WORKSPACE_NAME=MUXSDKImaListener
+readonly WORKSPACE_PATH=${TOP_DIR}/Example/${WORKSPACE_NAME}.xcworkspace
+
 set -euo pipefail
 
 brew install xcbeautify
 
-# reset simulators
+echo "▸ Using Xcode Version: ${XCODE_VERSION}"
+
+echo "▸ Resetting Simulators"
+
 xcrun -v simctl shutdown all
 xcrun -v simctl erase all
 
-echo "test xcode version"
-xcodebuild -version
-
 pushd Example
-#pod deintegrate && pod update
 
-xcodebuild -workspace MUXSDKImaListener.xcworkspace \
-           -scheme "MUXSDKImaListener-Example" \
-           -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.2' \
-           test \
-           | xcbeautify
+pod deintegrate && pod install
 
-xcodebuild -workspace MUXSDKImaListener.xcworkspace \
-           -scheme "DemoApp" \
-           -destination 'platform=iOS Simulator,name=iPhone 13,OS=16.2' \
+echo "▸ Executing Tests"
+
+xcodebuild -workspace ${WORKSPACE_PATH} \
+           -scheme "${SCHEME}" \
+           -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.4' \
            test \
            | xcbeautify
 
