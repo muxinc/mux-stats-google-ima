37dadb1c3883c8297c79086cb61912cd90f71fb9
AJ Lauer Barinov <abarinov@mux.com>
2023-09-08 12:11:50 -0700

    test: move Specta Expecta tests to application test target

--- Example/Pods/Target Support Files/Pods-DemoAppTests/Pods-DemoAppTests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-DemoAppTests/Pods-DemoAppTests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MUXSDKImaListener_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MUXSDKImaListener_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_DemoAppTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DemoAppTestsVersionString[];
 
