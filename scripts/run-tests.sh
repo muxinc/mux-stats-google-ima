e673c80c6b41d887ca9ed97c62969c038cc19d5a
AJ Lauer Barinov <abarinov@mux.com>
2024-01-03 15:31:36 -0800

    ci: add automated check for each of the example applications

---
+++ scripts/run-tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! command -v xcbeautify &> /dev/null
+then
+  echo -e "\033[1;31m ERROR: xcbeautify could not be found please install it... \033[0m"
+    exit 1
+fi
+
+echo "▸ Current Xcode: $(xcode-select -p)"
+
+readonly XCODE=$(xcodebuild -version | grep Xcode | cut -d " " -f2)
+
+echo "▸ Using Xcode Version: ${XCODE}"
+
+echo "▸ Available Xcode SDKs"
+xcodebuild -showsdks
+
+echo "▸ Running DemoApp tests"
+
+xcodebuild clean test \
+    -scheme DemoApp \
+    -workspace Example/MUXSDKImaListener.xcworkspace \
+    -destination 'platform=iOS Simulator,OS=17.2,name=iPhone 15' | xcbeautify
+
+echo "▸ Running MUXSDKIMATVOSExample tests"
+
+xcodebuild clean test \
+    -scheme MUXSDKIMATVOSExample \
+    -workspace Example/MUXSDKImaListener.xcworkspace \
+    -destination 'platform=tvOS Simulator,OS=17.2,name=Apple TV' | xcbeautify
