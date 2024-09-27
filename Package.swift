f523bd211141f27e0ae1ce7a7440d652f0eaf3b0
AJ Lauer Barinov <abarinov@mux.com>
2024-09-27 05:00:00 -0700

    chore: update dependencies, bump minor version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/muxinc/mux-stats-sdk-avplayer.git",
-            from: "4.0.0"
+            from: "4.1.0"
         ),
     ],
     targets: [
