cc826e2f57d8ca3077b6861a626f09d9f0a2e476
AJ Lauer Barinov <abarinov@mux.com>
2024-11-08 15:37:22 -0800

    chore: update example with additional ad variants and custom inputs (#46)

---
+++ Examples/MuxStatsGoogleIMAPluginSPMExampleIOS/MuxStatsGoogleIMAPluginSPMExampleIOS/MuxStatsGoogleIMAPluginSPMExampleIOSApp.swift
@@ -0,0 +1,15 @@
+//
+//  MuxStatsGoogleIMAPluginSPMExampleIOSApp.swift
+//  MuxStatsGoogleIMAPluginSPMExampleIOS
+//
+
+import SwiftUI
+
+@main
+struct MuxStatsGoogleIMAPluginSPMExampleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
